155b5b67d06b7dba907e49a8be31bea9cfe68c84
Troy Chryssos <troychryssos@gmail.com>
2020-03-30 17:45:38 -0700

    number input cleanup

--- src/components/NumberInput.js
+++ src/components/NumberInput.js
@@ -1,30 +1,20 @@
 import React from 'react'
-import { createUseStyles } from 'react-jss'
 
 import Input from 'components/Input'
-
-const useStyles = createUseStyles({
-	input: {
-
-	},
-})
 
 export default ({
 	setFormVals, formVals, formPath, label,
 	min, max, readOnly, labelColumn,
-}) => {
-	const classes = useStyles()
-	return (
-		<Input
-			type="number"
-			min={min}
-			max={max}
-			readOnly={readOnly}
-			formVals={formVals}
-			formPath={formPath}
-			setFormVals={setFormVals}
-			label={label}
-			labelColumn={labelColumn}
-		/>
-	)
-}
+}) => (
+	<Input
+		type="number"
+		min={min}
+		max={max}
+		readOnly={readOnly}
+		formVals={formVals}
+		formPath={formPath}
+		setFormVals={setFormVals}
+		label={label}
+		labelColumn={labelColumn}
+	/>
+)
