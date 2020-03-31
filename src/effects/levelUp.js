c423a3408e3d567e98f8bf8523631df2efe78d9b
Troy Chryssos <troychryssos@gmail.com>
2020-03-30 18:05:46 -0700

    new level up effect

--- src/effects/levelUp.js
+++ src/effects/levelUp.js
@@ -1,5 +1,5 @@
 import { useEffect } from 'react'
-import assoc from 'ramda/src/assoc'
+import mergeDeepRight from 'ramda/src/mergeDeepRight'
 import prop from 'ramda/src/prop'
 import split from 'ramda/src/split'
 
@@ -9,22 +9,18 @@ import { PROF_BONUS, TOTAL_HIT_DICE } from 'constants/schema'
 
 export default (formVals, setFormVals, level) => {
 	useEffect(() => {
-		// Update Proficiency Bonus
-		setFormVals(assoc(
-			PROF_BONUS,
-			profBonusCalc(level),
-			formVals,
-		))
-
-		// Update Hit Dice
 		const hitDiceSplit = split(
 			'd',
 			prop(TOTAL_HIT_DICE, formVals),
 		)
-		setFormVals(assoc(
-			TOTAL_HIT_DICE,
-			`${level}d${prop(1, hitDiceSplit)}`,
-			formVals,
-		))
+		setFormVals(
+			mergeDeepRight(
+				formVals,
+				{
+					[PROF_BONUS]: profBonusCalc(level),
+					[TOTAL_HIT_DICE]: `${level}d${prop(1, hitDiceSplit)}`,
+				},
+			),
+		)
 	}, [level])
 }
