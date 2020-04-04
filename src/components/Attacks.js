1acb4354ba81e1f4ab9059747ca9977d3dc9ace5
Troy Chryssos <troychryssos@gmail.com>
2020-04-03 21:53:46 -0700

    some styling on this attack form, but not much

--- src/components/Attacks.js
+++ src/components/Attacks.js
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react'
 import { createUseStyles } from 'react-jss'
+import clsx from 'clsx'
 import prop from 'ramda/src/prop'
 import remove from 'ramda/src/remove'
 import assoc from 'ramda/src/assoc'
@@ -13,22 +14,32 @@ import mapWithIndex from 'util/mapWithIndex'
 
 import SheetContext from 'contexts/sheetContext'
 import { ATTACKS } from 'constants/schema'
+import { black } from 'constants/styles/colors'
 
 const useStyles = createUseStyles({
 	attacksWrapper: {
 		display: 'flex',
 		flexDirection: 'column',
+		width: 'fit-content',
+		border: [[1, 'solid', black]],
+		padding: 4,
 	},
 	attackRow: {
 		display: 'flex',
 		alignItems: 'center',
-		marginBottom: 8,
+		paddingBottom: 8,
 		'&:last-child': {
-			marginBottom: 0,
+			paddingBottom: 0,
+		},
+		'&:hover $removeButton': {
+			display: 'block',
 		},
 	},
 	inputWrapper: {
 		marginRight: 4,
+	},
+	removeButton: {
+		display: 'none',
 	},
 })
 
@@ -39,11 +50,7 @@ const MappedAttacks = ({ attacks, formVals, classes }) => mapWithIndex(
 		const deleteAttack = () => setFormVals(
 			assoc(
 				ATTACKS,
-				remove(
-					index,
-					1,
-					attacks,
-				),
+				remove(index, 1, attacks),
 				formVals,
 			),
 		)
@@ -53,12 +60,6 @@ const MappedAttacks = ({ attacks, formVals, classes }) => mapWithIndex(
 				key={index}
 				className={classes.attackRow}
 			>
-				<div className={classes.inputWrapper}>
-					<Button
-						label="x"
-						onClick={deleteAttack}
-					/>
-				</div>
 				<div className={classes.inputWrapper}>
 					<TextInput
 						formPath={[ATTACKS, index, 'name']}
@@ -70,11 +71,16 @@ const MappedAttacks = ({ attacks, formVals, classes }) => mapWithIndex(
 						min={0}
 					/>
 				</div>
-				<div>
+				<div className={classes.inputWrapper}>
 					<TextInput
 						formPath={[ATTACKS, index, 'damage']}
 					/>
 				</div>
+				<Button
+					label="x"
+					onClick={deleteAttack}
+					className={classes.removeButton}
+				/>
 			</div>
 		)
 	},
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,14 +1,24 @@
 import React from 'react'
 import { createUseStyles } from 'react-jss'
+import clsx from 'clsx'
 
 const useStyles = createUseStyles({
-
+	button: {
+		width: 'fit-content',
+	},
 })
 
-export default ({ label, onClick }) => {
+export default ({ label, onClick, className }) => {
 	const classes = useStyles()
 	return (
-		<button type="button" onClick={onClick}>
+		<button
+			type="button"
+			onClick={onClick}
+			className={clsx(
+				classes.button,
+				className,
+			)}
+		>
 			{label}
 		</button>
 	)
