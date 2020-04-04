f10e1a05a5c58f409a1f7ab3f566f653f1355603
Troy Chryssos <troychryssos@gmail.com>
2020-04-03 21:18:08 -0700

    attack inputs display and add new when previous attack is full

--- src/components/Attacks.js
+++ src/components/Attacks.js
@@ -1,31 +1,86 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { createUseStyles } from 'react-jss'
-import map from 'ramda/src/map'
 import prop from 'ramda/src/prop'
+import path from 'ramda/src/path'
+import assocPath from 'ramda/src/assocPath'
+
+import TextInput from 'components/TextInput'
+import NumberInput from 'components/NumberInput'
+
+import mapWithIndex from 'util/mapWithIndex'
 
 import SheetContext from 'contexts/sheetContext'
 import { ATTACKS } from 'constants/schema'
 
 const useStyles = createUseStyles({
-
+	attackRow: {
+		display: 'flex',
+		marginBottom: 8,
+		'&:last-child': {
+			marginBottom: 0,
+		},
+	},
+	inputWrapper: {
+		marginRight: 4,
+	},
 })
 
-const MappedAttacks = ({ attacks, classes }) => map(
-	(attack) => {
-		const { name, attackBonus, damage } = attack
-		return (
-			<div>{name}{attackBonus}{damage}</div>
-		)
-	},
+const MappedAttacks = ({ attacks, classes }) => mapWithIndex(
+	(attack, index) => (
+		<div
+			key={index}
+			className={classes.attackRow}
+		>
+			<div className={classes.inputWrapper}>
+				<TextInput
+					formPath={[ATTACKS, index, 'name']}
+				/>
+			</div>
+			<div className={classes.inputWrapper}>
+				<NumberInput
+					formPath={[ATTACKS, index, 'attackBonus']}
+					min={0}
+				/>
+			</div>
+			<div>
+				<TextInput
+					formPath={[ATTACKS, index, 'damage']}
+				/>
+			</div>
+		</div>
+	),
 	attacks,
 )
 
 export default () => {
+	const [lastIndex, setLastIndex] = useState(0)
 	const classes = useStyles()
 	const formVals = useContext(SheetContext)
+	const { setFormVals } = formVals
+	const attacks = prop(ATTACKS, formVals)
+	const lastName = path([lastIndex, 'name'], attacks)
+	const lastDamage = path([lastIndex, 'damage'], attacks)
+
+	useEffect(() => {
+		if (lastName && lastDamage) {
+			setFormVals(
+				assocPath(
+					[ATTACKS, lastIndex + 1],
+					{
+						name: '',
+						attackBonus: 0,
+						damage: '',
+					},
+					formVals,
+				),
+			)
+			setLastIndex(lastIndex + 1)
+		}
+	}, [lastName, lastDamage])
+
 	return (
 		<MappedAttacks
-			attacks={prop(ATTACKS, formVals)}
+			attacks={attacks}
 			classes={classes}
 		/>
 	)
---
+++ src/util/mapWithIndex.js
@@ -0,0 +1,4 @@
+import map from 'ramda/src/map'
+import addIndex from 'ramda/src/addIndex'
+
+export default addIndex(map)
